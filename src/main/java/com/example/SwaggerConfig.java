3dba216bb9db7c9056b5daeb9edab01033044a1e
Dami Park <ekal0323@gmail.com>
2023-03-30 09:19:09 +0900

    swagger-ui 설정

---
+++ src/main/java/com/example/SwaggerConfig.java
@@ -0,0 +1,70 @@
+package com.example;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@Configuration
+//@EnableSwagger2
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2);
+//    }
+//    
+//    
+//    
+//}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private ApiInfo commonInfo() {
+        return new ApiInfoBuilder()
+                .title("PetProject")
+                //.description("")
+                //.license("leeys")
+                //.licenseUrl("http://leeys.tistory.com")
+                .version("1.0")
+                .build();
+    }
+
+    @Bean
+    public Docket imageApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("image")
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/app/api/image/**"))
+                .build()
+                .apiInfo(commonInfo());
+    }
+    @Bean
+    public Docket mainApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("main")
+                .useDefaultResponseMessages(false)
+                .select()
+                //.apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+                .paths(PathSelectors.ant("/app/"))
+                .build()
+                .apiInfo(commonInfo());
+    }
+}

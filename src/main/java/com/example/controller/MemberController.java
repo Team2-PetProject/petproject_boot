cb8597706b66d150471f3afc0395d62900f79afd
gjyal <gjyal@BOOK-KCHKG0HGU1>
2023-04-05 16:53:41 +0900

    아이디중복체크 수정

--- src/main/java/com/example/controller/MemberController.java
+++ src/main/java/com/example/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import java.nio.charset.Charset;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
@@ -9,10 +10,13 @@ import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -59,19 +63,32 @@ public class MemberController {
 		return new ResponseEntity<>("회원가입 성공", HttpStatus.OK);
 	}
 	
+	
+//	@RequestMapping("/idCheck")
+//	@ResponseBody
+//	public String idCheck() {
+//		return "";
+//	}//id가 없을 시 404 에러가 나서.....................ㅠㅠ
+	
 
 	@PostMapping("/idCheck/{mbId}")
 	@ApiOperation(value = "아이디 중복체크")
-	public ResponseEntity<Object> idCheck(@PathVariable("mbId") String mbId) {
+	public ResponseEntity<String> idCheck(@PathVariable("mbId") String mbId) {
 		System.out.println("/idCheck 주소 : " + mbId);
 		
 		MemberDTO memberDTO = service.mypage(mbId);
+		System.out.println("/idCheck " + memberDTO);
 		String mesg = "아이디 사용가능";
+//		HttpHeaders header = new HttpHeaders();
+//		header.setContentType(new MediaType("application","json", Charset.forName("UTF-8")));
 		if(memberDTO!=null) {
 			mesg ="아이디 중복";
-			return new ResponseEntity<>(mesg, HttpStatus.CONFLICT);
+			return new ResponseEntity<String>(mesg, HttpStatus.OK);  
+			
+		}else {
+			return new ResponseEntity<String>(mesg,HttpStatus.OK);
 		}
-		return new ResponseEntity<>(mesg, HttpStatus.OK);
+		
 		
 	}
 	

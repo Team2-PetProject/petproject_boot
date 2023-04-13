c3c25c5dc8ee5503becda363b91171f7cdd442ed
유재건 <jamy78@naver.com>
2023-04-13 13:01:40 +0900

    머지

--- src/main/java/com/example/common/constant/ComConstant.java
+++ src/main/java/com/example/common/constant/ComConstant.java
@@ -1,7 +1,7 @@
 package com.example.common.constant;
 
 public class ComConstant {
-	
+
 	public static final String MEMBER_ID = "memberInfo";
 	public static final Integer DEFAULT_CURPAGE = 1;
 	public static final Integer DEFAULT_PERPAGE = 4;

96094a00ff1a6c822f02310ceacdb3edf647c405
Dami Park <ekal0323@gmail.com>
2023-04-17 13:06:47 +0900

    build 설정

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -2,11 +2,17 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 	
+	@RequestMapping("/")
+	public String vueTest() {
+		System.out.println("/");
+		return "index";
+	}
 	@GetMapping("/main")
 	public String mainpage() {
 		System.out.println("/main페이지");
